7f4c1f3a8eb0cf908c77cf4f055faeb934ec6537
Manel <walletdans@gmail.com>
2024-07-02 23:35:21 +0200

    last commit

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,40 @@
+//check username, password in post (login) request
+// if exist create new JWT
+// send back to front end
+
+// setup authentication so only the request with JWT can acces the dashboard
+
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+const customAPIerror = require('../errors/custom-error')
+
+
+const login = async (req,res) => {
+    const {username, password} = req.body
+    console.log(username, password)
+    if(!username || !password){
+        throw new CustomAPIError('please provide email and password', 400)
+    }
+    
+    // just for demo, nomarlly provided by database
+const id = new Date().getDate()
+
+
+//try to keep the payload small, better experience for user
+
+const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+
+res.status(200).json({msg: 'user created',token})
+}
+
+
+
+
+const dashboard = async (req,res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: 'Hello, John Doe', secret: ' Herre is your authorized data, your lucky number is ${luckyNumber}' })
+}
+
+module.exports = {
+    login,dashboard
+}
