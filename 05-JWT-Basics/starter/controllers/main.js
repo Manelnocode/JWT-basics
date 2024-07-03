213611dfe9b8195b53b49237a462e6407737e794
Manel <walletdans@gmail.com>
2024-07-03 16:40:01 +0200

    last commit

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -6,7 +6,10 @@
 
 const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
-const customAPIerror = require('../errors/custom-error')
+
+
+// Hacemos una peticion post desde postman en la que enviamos un body con el nombre y la contraseña.
+// Creamos un id y despues un token que contendra el id + usuario + secret con el que firmaremos el token y como respuesta enviaremos la linea 27. 
 
 
 const login = async (req,res) => {
@@ -15,13 +18,10 @@ const login = async (req,res) => {
     if(!username || !password){
         throw new CustomAPIError('please provide email and password', 400)
     }
-    
     // just for demo, nomarlly provided by database
 const id = new Date().getDate()
 
-
 //try to keep the payload small, better experience for user
-
 const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
 
 res.status(200).json({msg: 'user created',token})
@@ -31,8 +31,25 @@ res.status(200).json({msg: 'user created',token})
 
 
 const dashboard = async (req,res) => {
+    
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided',401)
+    }
+
+    const token = authHeader.split(' ')[1]
+    
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded);
+    } catch (error) {
+        throw new CustomAPIError('No authorized to acces this route',401)
+    }
+    
+
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: 'Hello, John Doe', secret: ' Herre is your authorized data, your lucky number is ${luckyNumber}' })
+    res.status(200).json({msg: `Hello, John Doe', secret: ' Herre is your authorized data, your lucky number is ${luckyNumber}` })
 }
 
 module.exports = {
